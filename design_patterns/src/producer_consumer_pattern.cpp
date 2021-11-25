a8fcdc1b1f4d071c2ea4233881a2ff5be0152445
Lu Gao <1695658314@qq.com>
2021-11-25 15:04:49 +0800

    phread debug

--- design_patterns/src/producer_consumer_pattern.cpp
+++ design_patterns/src/producer_consumer_pattern.cpp
@@ -14,17 +14,17 @@ namespace pc {
 
 void* consumer(void* arg) {
     int* num = (int*)arg;
-    while (1) {
+    /*while (1)*/ {
         // lock
         pthread_mutex_lock(&g_mutex);
 
         while (shared_var == 0) {
-            std::cout << "consumer " << num << "begin wait a condition...\n";
+            std::cout << "consumer " << *num << " begin wait a condition...\n";
             pthread_cond_wait(&g_cond, &g_mutex); // block here util producer notify
         }
 
-        std::cout << "consumer " << num << "end wait a condition...\n";
-        std::cout << "consumer " << num << "begin consume product...\n";
+        std::cout << "consumer " << *num << " end wait a condition...\n";
+        std::cout << "consumer " << *num << " begin consume product...\n";
 
         shared_var--;
 
@@ -39,16 +39,16 @@ void* consumer(void* arg) {
 void* producer(void* arg) {
     int* num = (int*)arg;
 
-    while(1) {
+    /*while (1)*/ {
 
         pthread_mutex_lock(&g_mutex);
 
-        std::cout << "producer " << num << "begin product...\n";
+        std::cout << "producer " << *num << " begin product...\n";
         shared_var++;
-        std::cout << "producer " << num << "end product...\n";
+        std::cout << "producer " << *num << " end product...\n";
 
         pthread_cond_signal(&g_cond);
-        std::cout << "producer " << num << "notify consumer by cond var...\n";
+        std::cout << "producer " << *num << " notify consumer by cond var...\n";
 
         pthread_mutex_unlock(&g_mutex);
         sleep(2);
@@ -64,23 +64,23 @@ void test() {
     pthread_cond_init(&g_cond, nullptr);
 
     for (int i = 0; i < CONSUMERS_CNT; i++) {
-        pthread_create(&g_thread[i], nullptr, consumer, (void*)i);
+        pthread_create(&g_thread[i], nullptr, consumer, (void*)&i);
+        sleep(1);
     }
 
-    sleep(1);
+    sleep(2);
 
-    for (int i = 0; i < PRODUCERS_CNT; i++) {
-        pthread_create(&g_thread[i], nullptr, producer, (void*)i);
+    for (int i = CONSUMERS_CNT; i < (PRODUCERS_CNT + CONSUMERS_CNT); i++) {
+        pthread_create(&g_thread[i], nullptr, producer, (void*)&i);
+        sleep(1);
     }
 
-    for (int i = 0; i < CONSUMERS_CNT + PRODUCERS_CNT; i++) {
+    for (int i = 0; i < (CONSUMERS_CNT + PRODUCERS_CNT); i++) {
         pthread_join(g_thread[i], nullptr);
     }
 
     pthread_mutex_destroy(&g_mutex);
     pthread_cond_destroy(&g_cond);
-
-
 
 
 }
